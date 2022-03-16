77f4237fc59db8181702007991d66af84653a9df
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2022-03-16 18:08:41 -0500

    shop grid

--- shop.php
+++ shop.php
@@ -11,11 +11,41 @@
 </head>
 
 <body>
-  
+
+  <div class="container">
+    <?php
+  # get products from database
+
+  for ($i=0; $i < 3; $i++) { 
+    ?>
+    <div class="row justify-content-evenly">
+      <?php
+    for ($j=0; $j < 3; $j++) { 
+    ?>
+
+      <div class="col-3">
+
+        <div class="card">
+          <h3 class="card-header">Header</h3>
+          <img class="card-img-top" src="images/placeholder.jpg" alt="image" width="100" height="100">
+          <div class="card-body">
+            <h3 class="card-title">Card Title</h3>
+            <div class="card-text">Card Content</div>
+          </div>
+        </div>
+
+      </div>
+
+      <?php } ?>
+
+    </div>
+    <?php } ?>
+
+  </div>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-    crossorigin="anonymous"></script>
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+  </script>
 </body>
 
 </html>

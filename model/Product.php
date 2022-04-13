fd6e0696ad349f60d8965094b416036712e36f52
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2022-04-12 22:04:19 -0500

    get products from product table:

--- model/Product.php
+++ model/Product.php
@@ -10,22 +10,23 @@ class Product {
 }
 
 function getProducts() {
-  include_once("../configuration.php");
+  include("configuration.php");
 
-  // $conn = new mysqli($servername, $db_username, $db_password, $default_db);
-  // $sql = "SELECT * FROM product_info";
-  // $stmt = $conn->prepare($sql);
-  // echo $stmt;
-  // $stmt->bind_result($id, $name, $price, $type, $quantity);
-  // $stmt->execute();
-
-  // while ($stmt->fetch()) {
-  //   array_push($products, new Product($name, $price));
-  // }
+  $conn = new mysqli($servername, $db_username, $db_password, $default_db);
+  $sql = "SELECT * FROM product_info";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_result($id, $name, $price, $type, $quantity);
+  $stmt->execute();
 
   $products = [];
-  for ($i=0; $i < 10; $i++) { 
-    array_push($products, new Product("Product", 9.99));
+  while ($stmt->fetch()) {
+    array_push($products, new Product($name, $price));
   }
+  $conn->close();
+
+  // $products = [];
+  // for ($i=0; $i < 10; $i++) { 
+  //   array_push($products, new Product("Product", 9.99));
+  // }
   return $products;
 }
